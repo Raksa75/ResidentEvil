bb48f61babbdeb7c3d86bbc765e6e692dd15e3f7
Hades91000 <147702032+Hades91000@users.noreply.github.com>
2023-12-20 15:44:48 +0100

    Level design + fix
    Level design exterieur (reste interieur + light et camera a ajuster)
    - Restart du level lorsque l'ia nous foncer dessus
    - Ajout de mesh sur le level et sur l'IA

--- Assets/Script/Chase_Player.cs
+++ Assets/Script/Chase_Player.cs
@@ -28,23 +28,16 @@ public class ChasePlayer : MonoBehaviour
         }
     }
 
-    void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider collision)
     {
-        // Gérez la collision avec le joueur
-        if (other.CompareTag("Player"))
+        if (collision.tag == "Player")
         {
-            // Redémarrez le niveau (vous pouvez remplacer cela par le code de redémarrage approprié)
-            RestartLevel();
+            SceneManager.LoadSceneAsync(0);
         }
     }
-
-    void RestartLevel()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        //SceneManager.LoadScene("SampleScene");
-        // À remplacer par votre logique de redémarrage de niveau
-        // Par exemple, rechargez la scène ou réinitialisez la position du joueur et de l'IA
-        Debug.Log("Level Restarted");
-       
-    }
 }
+
+
+
+
+
